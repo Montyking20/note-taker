edbb075f182a30fba4ea49b96ed047a9c70ede06
Chris <montyking20@gmail.com>
2022-03-15 01:31:20 -0600

    added server.js - fixed code

---
+++ server.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+const express = require('express');
+const { notes }= require('./db/db.json');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+const path = require('path')
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+function filterByQuery(query, notesArray) {
+  let filteredResults = notesArray;
+  if (query.title) {
+      filteredResults = filteredResults.filter(notes => notes.title === query.title);        
+  }
+  if (query.text) {
+      filteredResults = filteredResults.filter(notes => notes.text === query.text);
+  }
+  // return the filtered results:
+  return filteredResults;
+}
+
+function findByID(id, notesArray) {
+  const result = notesArray.filter(notes => notes.id === id)[0];
+  return result;
+}
+
+// Create new notes
+function createNewNotes(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify({ notes: notesArray}, null, 2)
+  );
+  // return finished code to post route for response
+  return note;
+}
+
+// Check if each key not only exists, but that it is also the right type of data.
+function validateNote(note) {
+  if (!note.title || typeof note.title !== 'string') {
+      return false;
+  }
+  if (!note.text || typeof note.text !== 'string') {
+      return false;
+  }
+  return true;
+}
+
+app.get('/api/notes', (req, res) => {
+  let results = notes;
+  if (req.query) {
+      results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+// REMEMBER: This route should take us to /notes
+app.get('/notes', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const result = findByID(req.params.id, notes);
+  res.json(result);
+});
+
+app.post('/api/notes', (req, res) => {
+  // This method will only work as long as we don't remove any data from notes.json. If we do, the id numbers will be thrown off and we'll end up with a duplicate value at some point.
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+  if (!validateNote(req.body)) {
+      res.status(400).send('The note is not properly formatted.');
+  } else {
+      // Add notes to json file and notes array
+      const note = createNewNotes(req.body, notes);
+      //req.body is where our incoming content will be
+      res.json(note);
+  }
+  // Add notes to json file and notes array 
+  // const note = createNewNotes(req.body, notes)
+
+  //req.body is where our incoming content will be
+  // res.json(req.body);
+});
+
+// '/' brings us to the root route of the server! This is the route used to create a homepage for a server.
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+// Reminder: app.listen should always be last. 
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
